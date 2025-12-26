0879a99a07e6a40a15cef2039ed1af9ec9f19f1d
Pascal <mup0117@gmail.com>
2025-12-26 12:05:51 +0100

    Fix inference return value.

--- game/inference/__init__.py
+++ game/inference/__init__.py
@@ -1,5 +1,6 @@
 from .predictor import Predictor
+from .detector import Detector
 from .base import Inference
 from . import utils
 
-__all__ = ['Predictor', 'Inference', 'utils']
+__all__ = ['Predictor', 'Inference', 'utils', 'Detector']
